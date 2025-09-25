b919cce28d8c5add89bab0a350ed47149318f33b
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-09-25 08:32:43 -0600

    se creo el registrar de usuario

---
+++ FrontendG5A/DTO/UsuarioRegistrarDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontendG5A.DTO
+{
+    public class UsuarioRegistrarDto
+    {
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [EmailAddress(ErrorMessage = "El correo debe ser válido")]
+        [Required(ErrorMessage = "El correo de usuario es requerido")]
+        public string Correo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        
+        public int Estado { get; set; } = 1; // Por defecto activo
+        public int IdRol { get; set; }
+    }
+}
